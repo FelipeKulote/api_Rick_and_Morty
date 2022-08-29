e6034a043d2fa09b4942443bd2c0883b5ad0557b
Felipe Augusto <lipe_augustobq@hotmail.com>
2022-08-29 17:28:28 -0300

    fix: Correção de bugs

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-import express from "express";
+import express, { Router } from "express";
 import cors from "cors";
 
-import{ MongoDbConnection } from "./database/repository/mongo/connection/connect.js"
-import { makeCharacterFactory } from "./factories/character.js"
-import { makeUserFactory } from "./factories/user.js"
+import { MongoDbConnection } from "./database/repository/mongo/connection/connect.js";
+import { makeCharacterFactory } from "./factories/character.js";
+import { makeUserFactory } from "./factories/user.js";
 
 const ConnectDb = new MongoDbConnection();
 await ConnectDb.ConnectDb();
 
 const app = express();
-const router = router();
+const router = Router();
 
 const user = makeUserFactory(router);
 const character = makeCharacterFactory(router);
@@ -21,5 +21,5 @@ app.use("/users", user.route());
 app.use("/character", character.route());
 
 app.listen(3000, () => {
-  console.log("Servidor rodando em: http://localhost:3000")
+  console.log("Servidor rodando em: http://localhost:3000");
 });
