afb9de4ea32298de4e4e4c2435a10c77046aa042
Felipe Augusto <lipe_augustobq@hotmail.com>
2022-08-29 21:54:44 -0300

    feat: criação da auth midleware

---
+++ midlewares/auth.js
@@ -0,0 +1,21 @@
+import { JwtHelper } from "../auth/jwt";
+import { UserRepositoryMongoDb } from "../database/repository/userRepository.js";
+import { FindUserByIdUseCase } from "../services/usecases/user/findUserById.js";
+
+export async function authentication(req, res, next) {
+  const token = req.headers.authorization;
+  const jwt = new JwtHelper();
+
+  try {
+    const userData = jwt.verifyToken(token.slice(7));
+
+    const repository = new UserRepositoryMongoDb();
+    const findUserById = FindUserByIdUseCase(repository);
+
+    await findUserById.execute(userData.data.id);
+    next();
+  } catch (err) {
+    console.log(err);
+    res.status(401).send("Authentication failed, token is invalid");
+  }
+}
